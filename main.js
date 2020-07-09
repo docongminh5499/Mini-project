998a7947f26b57ba3fc119d8d3ac778657f2d3f9
Minh <meo123zzz@gmail.com>
2020-07-09 19:34:27 +0700

    Reset mini-project

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 
 var gv = gv || {};
 
-var DESIGN_RESOLUTION_WIDTH = 640;
-var DESIGN_RESOLUTION_HEIGHT = 1136;
-var DISPLAY_FPS = true;
-
+var DESIGN_RESOLUTION_WIDTH = 1136;
+var DESIGN_RESOLUTION_HEIGHT = 640;
 cc.game.onStart = function () {
     if (!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
         document.body.removeChild(document.getElementById("cocosLoading"));
@@ -12,26 +10,25 @@ cc.game.onStart = function () {
     cc.view.enableRetina(true);
     // Adjust viewport meta
     cc.view.adjustViewPort(true);
-    // jsb: the main class which manages interaction with JavaScript environment
     jsb.fileUtils.addSearchPath(fr.NativeService.getFolderUpdateAssets(), true);
     jsb.fileUtils.addSearchPath(fr.NativeService.getFolderUpdateAssets() + "/res", true);
+
+    jsb.fileUtils.addSearchPath(fr.NativeService.getFolderUpdateAssets() + "/res/myRes", true);
 
     cc.loader.resPath = "res";
     cc.LoaderScene.preload(g_resources, function () {
         //hide fps
-        cc.director.setDisplayStats(DISPLAY_FPS);
+        cc.director.setDisplayStats(true);
         // Setup the resolution policy and design resolution size
         var frameSize = cc.view.getFrameSize();
         var ratio = frameSize.width/frameSize.height;
         if(ratio < 2){
-            // Portrait
-            cc.view.setDesignResolutionSize(DESIGN_RESOLUTION_WIDTH, DESIGN_RESOLUTION_HEIGHT, cc.ResolutionPolicy.FIXED_HEIGHT);
-        }else {
-            // Landscape
-            cc.view.setDesignResolutionSize(DESIGN_RESOLUTION_HEIGHT, DESIGN_RESOLUTION_HEIGHT / 2, cc.ResolutionPolicy.SHOW_ALL);
+            cc.view.setDesignResolutionSize(DESIGN_RESOLUTION_WIDTH,DESIGN_RESOLUTION_HEIGHT, cc.ResolutionPolicy.FIXED_HEIGHT);
+        }else{
+            cc.view.setDesignResolutionSize(DESIGN_RESOLUTION_WIDTH,DESIGN_RESOLUTION_WIDTH/2, cc.ResolutionPolicy.SHOW_ALL);
         }
 
-        // The game will be resized when browser size change --> web
+        // The game will be resized when browser size change
         cc.view.resizeWithBrowserSize(true);
         //socket
         gv.gameClient = new GameClient();
@@ -43,5 +40,3 @@ cc.game.onStart = function () {
     }, this);
 };
 cc.game.run();
-
-// test
